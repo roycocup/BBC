6eb8b9cbe1a955199b7949345ae23aa1bfd99421
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-10-08 14:59:24 +0100

    Testing that input is valid

--- src/main/java/uk/co/rodderscode/bbc/Fetcher.java
+++ src/main/java/uk/co/rodderscode/bbc/Fetcher.java
@@ -1,6 +1,10 @@
 package uk.co.rodderscode.bbc;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Fetcher {
 
@@ -10,17 +14,26 @@ public class Fetcher {
         Fetcher fetcher = new Fetcher();
     }
 
-    public String getInput()
+    public ArrayList<String> getInput()
     {
 
         Scanner input = new Scanner( System.in );
-        StringBuilder lines = new StringBuilder();
+        ArrayList<String> output = new ArrayList<>();
 
-        while(input.hasNext()){
-            lines.append(input.nextLine());
+        while(input.hasNextLine()){
+            String entry = input.nextLine();
+            if (entry.matches("^(\\d).*?"))
+            {
+                continue;
+            }
+            if (entry.matches("^.+?(\\s)+.+?$"))
+            {
+                continue;
+            }
+            output.add(entry);
         }
 
-        return lines.toString();
+        return output;
     }
 
 
--- src/test/java/TestInput.java
+++ src/test/java/TestInput.java
@@ -4,23 +4,123 @@ import uk.co.rodderscode.bbc.Fetcher;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.*;
 
 public class TestInput{
 
+
+    /**
+     *
+     * We should expect an array of Strings each one with a new line terminated word.
+     * If there is a space between 2 words the input should be ignored as is malformed.
+     * Spaces can only exist before or after the word in which case they should be trimmed
+     */
+
     @Test
     public void canSetStdIn()
     {
-        String expected = "this is a string";
+        String expected = "this-is-a-string";
+
         InputStream inputStream = new ByteArrayInputStream(expected.getBytes());
 
         System.setIn(inputStream);
 
         Fetcher fetcher = new Fetcher();
-        String inputText = fetcher.getInput();
+        ArrayList<String> inputText = fetcher.getInput();
 
-        assertEquals(expected, inputText);
 
+        assertEquals(expected, inputText.toString().replace("[", "").replace("]", ""));
+
+    }
+
+
+    @Test
+    public void inputReturnsSameArrayOfString()
+    {
+
+        // each one of these entries represents a batch of inputs from the user
+        String[] listOfStrings = {"string-one", "string-two", "string-three"};
+
+
+        StringBuilder fakeInput = new StringBuilder();
+
+        // fake input should be a string of newline separated strings
+        for(String line : listOfStrings)
+        {
+            fakeInput.append(line);
+            fakeInput.append("\\n");
+        }
+
+        InputStream inputStream = new ByteArrayInputStream(fakeInput.toString().getBytes());
+
+        // setting the input stream with the entry
+        System.setIn(inputStream);
+
+
+        Fetcher fetcher = new Fetcher();
+
+        ArrayList<String> output = fetcher.getInput();
+
+        assertEquals ("["+fakeInput.toString()+"]", output.toString());
+
+
+    }
+
+    @Test
+    public void inputShouldTrimWhiteSpaces()
+    {
+        // each one of these entries represents a batch of inputs from the user
+        String[] validEntries = {
+                "somestring",
+                "http://anormal.url",
+                "string-with-space-at-end ",
+                " string-with-initial-space",
+                "http://someurl\\nhttp://another.url\\n"
+        };
+
+
+        // testing valid entries
+        for(String expected : validEntries)
+        {
+            InputStream inputStream = new ByteArrayInputStream(expected.getBytes());
+
+            // setting the input stream with the entry
+            System.setIn(inputStream);
+
+            Fetcher fetcher = new Fetcher();
+
+            ArrayList<String> output = fetcher.getInput();
+
+            assertEquals ("["+expected.toString()+"]", output.toString());
+        }
+
+    }
+
+    @Test
+    public void inputShouldIgnoreInvalidEntries()
+    {
+        String[] invalidEntries = {
+                "string with spaces",
+                "100string-starting-with-number",
+        };
+
+        // testing invalid entries
+        for(String expected : invalidEntries)
+        {
+            InputStream inputStream = new ByteArrayInputStream(expected.getBytes());
+
+            // setting the input stream with the entry
+            System.setIn(inputStream);
+
+            Fetcher fetcher = new Fetcher();
+
+            ArrayList<String> output = fetcher.getInput();
+
+            assertEquals ("[]", output.toString());
+        }
     }
 
 }
