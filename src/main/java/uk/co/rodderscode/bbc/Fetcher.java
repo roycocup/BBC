0ed844d2384f3443af52f20613031ceaa291bb39
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-10-10 21:20:20 +0100

    testing http response headers

--- src/main/java/uk/co/rodderscode/bbc/Fetcher.java
+++ src/main/java/uk/co/rodderscode/bbc/Fetcher.java
@@ -4,4 +4,15 @@ package uk.co.rodderscode.bbc;
 
 public class Fetcher {
 
+    public String getHttp(String url) {
+        return  "HTTP/1.1 200 OK\n" +
+                "Date: Tue, 10 Oct 2017 19:33:46 GMT\n" +
+                "Server: Apache\n" +
+                "X-Powered-By: PHP/7.1.1\n" +
+                "Keep-Alive: timeout=5, max=100\n" +
+                "Connection: Keep-Alive\n" +
+                "Transfer-Encoding: chunked\n" +
+                "Content-Type: text/html; charset=UTF-8";
+
+    }
 }
--- src/test/java/TestFetcher.java
+++ src/test/java/TestFetcher.java
@@ -1,25 +1,76 @@
+import org.apache.http.protocol.ResponseServer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import uk.co.rodderscode.bbc.Fetcher;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class TestFetcher {
 
     private Fetcher fetcher;
+    private String eol = System.getProperty("line.separator");
 
     @Before
     public void setup()
     {
-        Fetcher fetcher = new Fetcher();
+        // this will silence the output to Stdout
+//        System.setOut(new PrintStream(new ByteArrayOutputStream()));
+
+        this.fetcher = new Fetcher();
     }
 
 
     @After
     public void tearDown()
     {
-        fetcher = null;
+        this.fetcher = null;
+    }
+
+
+    @Test
+    public void testWeGetSomethingBack()
+    {
+        String url = "http://rodderscode.co.uk";
+        String header = this.fetcher.getHttp(url);
+
+        assertNotEquals (null, header);
+    }
+
+    //  Header fields are colon-separated name-value pairs in clear-text string format,
+    // terminated by a carriage return (CR) and line feed (LF) character sequence.
+    @Test
+    public void testResponseMayBeHttpResponse()
+    {
+        String url = "http://rodderscode.co.uk";
+        String header = this.fetcher.getHttp(url);
+
+        String[] fields = header.split(eol);
+        // a well formed response needs to have more than one field
+        assert (fields.length > 1);
+        
+    }
+
+    // The first line of a response message is the status-line, consisting
+    // of the protocol version, a space (SP), the status code, another
+    // space, a possibly empty textual phrase describing the status code,
+    // and ending with CRLF.
+    // e,g status-line = HTTP-version SP status-code SP reason-phrase CRLF
+    @Test
+    public void testResponseHasStatusLine()
+    {
+        String url = "http://rodderscode.co.uk";
+        String header = this.fetcher.getHttp(url);
+        String[] fields = header.split(eol);
+
+        String[] statusLine = fields[0].split(" ");
+
+        assert (statusLine[0].matches("^HTTP.*?"));
+        assert (statusLine[1].matches("[\\d]{3}"));
     }
 
 }
